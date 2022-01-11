f0da4b57b6aa14721da0c328e59cd0cb0430c627
Zasik <mitya.leonov@gmail.com>
2022-01-11 20:21:02 +0100

    Basic Files

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Dmitry Zasenko on 11.01.22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
